1f549d04741c6ebf56d2ef7cf1bb5d54bd94c908
Joakim Persson <ljpersson16@gmail.com>
2017-10-18 19:20:38 +0200

    Kort commit för flyttning till annan dator

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
 require_once 'lib/limonade.php';
 
-dispatch_get('/', 'hello');
+//Redirects
+dispatch_get('/', 'index');
+//Version 1 API calls
+dispatch_get('/api/v1/:ingredients', 'findRecipes');
+dispatch_get('/api/v1/:recipe','findRecipe');
 
-function hello()
+function index()
 {
-  return html('html/index.html');
+   return html('html/index.html');
+}
+function findRecipe()
+{
+   $recipe = params('recipe');
+   //TODO: find recipe according to search
+}
+function findRecipes()
+{
+   $ingredients = params('ingredients');
+   //TODO: find recipes according to what ingredients user has
 }
 
 run();
