6ea1dd34ff48b32c136c919f8d6ee7abc977a861
Joakim Persson <ljpersson16@gmail.com>
2017-11-27 18:06:07 +0100

    ...?

--- index.php
+++ index.php
@@ -4,16 +4,27 @@ require_once 'lib/limonade.php';
 //Redirects
 dispatch_get('/', 'index');
 //Version 1 API calls
-dispatch_get('/api/v1/:ingredients', 'findRecipes');
-dispatch_get('/api/v1/:recipe','findRecipe');
+dispatch_get('/api/v1/recipes', 'X');  //Returnerar alla recept
+//dispatch_get('/api/v1/recipes/:id','X');  //Returnerar receptet som matchar id
+dispatch_get('/api/v1/recipes/:ingredients','findRecipe');   //Returnerar alla recept som innehåller ingredients
+dispatch_get('/api/v1/recipes/:ingredients/:acc','X'); //Returnerar alla recept som innehåller ingredients med minimum accuracy acc
+dispatch_get('/api/v1/ingredients','X');  //Returnerar alla ingredienser
+dispatch_post('/api/v1/recipes:recipe:ingredients','X'); //SKapa nytt recept
+dispatch_post('/api/v1/ingredients:ingredient','X');  //Skapa ny ingrediens
 
 function index()
 {
    return html('html/index.html');
 }
-function findRecipe()
+function findRecipe($ing)
 {
-   $recipe = params('recipe');
+   //echo $ing."\n";
+   $recipies = $_GET["recipe"];
+   foreach($recipies as $index => $r) {
+      echo $index.": ".$r."<br />";
+   }
+   //$recipe = params('recipe');
+   //echo $recipe;
    //TODO: find recipe according to search
 }
 function findRecipes()
