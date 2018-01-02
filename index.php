4c4bf9b75908c5e0444e9f096c4bf996c17c6c7b
Unknown <ljpersson16@gmail.com>
2018-01-02 15:15:36 +0100

    ~ Omformattering på kommentarer

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ require_once 'lib/limonade.php';
 require("env.php");
 
 //Version 1 API calls
-dispatch_get('/api/v1/recipes', 'getRecipes');                                //Returnerar alla recept
-dispatch_get('/api/v1/recipes/:id','getRecipeById');                          //Returnerar recept med id :id
-// dispatch_get('/api/v1/recipes/ingredients','getRecipesByIngredients');         //Returnera recept som innehåller ingredient, kräver ?ingredient[]=x
-dispatch_get('/api/v1/recipes/:id/:acc','getRecipesByIngsFilterAcc');  //Returnera recept som innehåller ingredient, kräver ?ingredient[]=x, :acc ger procentvärde
-dispatch_get('/api/v1/ingredients','getIngredients');                         //Returnerar alla ingredienser
-dispatch_post('/api/v1/recipes','createRecipe');                              //Skapa nytt recept, kräver headers
-dispatch_post('/api/v1/ingredients','createIngredient');                      //Skapa ny ingrediens, kräver headers
+dispatch_get('/api/v1/recipes', 'getRecipes');                              //Returnerar alla recept
+dispatch_get('/api/v1/recipes/:id','getRecipeById');                        //Returnerar recept med id :id
+// dispatch_get('/api/v1/recipes/ingredients','getRecipesByIngredients');   //Returnera recept som innehåller ingredient, kräver ?ingredient[]=x
+dispatch_get('/api/v1/recipes/:id/:acc','getRecipesByIngsFilterAcc');       //Returnera recept som innehåller ingredient, kräver ?ingredient[]=x, :acc ger procentvärde
+dispatch_get('/api/v1/ingredients','getIngredients');                       //Returnerar alla ingredienser
+dispatch_post('/api/v1/recipes','createRecipe');                            //Skapa nytt recept, kräver headers
+dispatch_post('/api/v1/ingredients','createIngredient');                    //Skapa ny ingrediens, kräver headers
 
 run();
 ?>
